630e808c05c1b60cce02c930ad43d9aa0e1ae8c1
JurajChovan <chovan@hotmail.com>
2021-03-30 11:29:09 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
+    { title: 'Home', url: 'home', icon: 'home' },
+    { title: 'Profile', url: 'profile', icon: 'person-circle' },
+    { title: 'All buddies', url: 'buddies', icon: 'people-circle' },
+    { title: 'Accepted', url: 'accepted', icon: 'people-circle' },
+    { title: 'Waited', url: 'waited', icon: 'people-circle' },
+    { title: 'Removed', url: 'removed', icon: 'people-circle' },
+    { title: 'Banned', url: 'banned', icon: 'people-circle' },
+    { title: 'Tags', url: 'mytags', icon: 'pricetags' },
+    { title: 'Me on lists', url: 'me', icon: 'information-circle' },
+    { title: 'Position', url: 'myposition', icon: 'locate' },
+    { title: 'Search', url: 'searchbytag', icon: 'search' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
