4ed38355fcf46c8147f0f443eaa7f2e767b6ef97
JurajChovan <chovan@hotmail.com>
2021-03-30 11:28:58 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,98 @@
+/* ------------------------------------------------------------------------------------------------------------ */
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
+/* ------------------------------------------------------------------------------------------------------------ */
 const routes: Routes = [
+  /* ------------------------------------------------------------------------------------------------------------ */
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
+  /* ------------------------------------------------------------------------------------------------------------ */
   {
     path: 'folder/:id',
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+  },
+  /* ------------------------------------------------------------------------------------------------------------ */
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  /* ------------------------------------------------------------------------------------------------------------ */
+  {
+    path: 'profile',
+    loadChildren: () => import('./pages/profile/profile.module').then( m => m.ProfilePageModule)
+  },
+  /* ------------------------------------------------------------------------------------------------------------ */
+  {
+    path: 'mytags',
+    loadChildren: () => import('./pages/mytags/mytags.module').then( m => m.MytagsPageModule)
+  },
+  /* ------------------------------------------------------------------------------------------------------------ */
+  {
+    path: 'myposition',
+    loadChildren: () => import('./pages/myposition/myposition.module').then( m => m.MypositionPageModule)
+  },
+  /* ------------------------------------------------------------------------------------------------------------ */
+  {
+    path: 'accepted',
+    loadChildren: () => import('./pages/accepted/accepted.module').then( m => m.AcceptedPageModule)
+  },
+  /* ------------------------------------------------------------------------------------------------------------ */
+  {
+    path: 'waited',
+    loadChildren: () => import('./pages/waited/waited.module').then( m => m.WaitedPageModule)
+  },
+  /* ------------------------------------------------------------------------------------------------------------ */
+  {
+    path: 'removed',
+    loadChildren: () => import('./pages/removed/removed.module').then( m => m.RemovedPageModule)
+  },
+  /* ------------------------------------------------------------------------------------------------------------ */
+  {
+    path: 'banned',
+    loadChildren: () => import('./pages/banned/banned.module').then( m => m.BannedPageModule)
+  },
+  /* ------------------------------------------------------------------------------------------------------------ */
+  {
+    path: 'me',
+    loadChildren: () => import('./pages/me/me.module').then( m => m.MePageModule)
+  },
+  /* ------------------------------------------------------------------------------------------------------------ */
+  {
+    path: 'buddy/:BuddyID',
+    loadChildren: () => import('./pages/buddy/buddy.module').then( m => m.BuddyPageModule)
+  },
+  /* ------------------------------------------------------------------------------------------------------------ */
+  {
+    path: 'buddies',
+    loadChildren: () => import('./pages/buddies/buddies.module').then( m => m.BuddiesPageModule)
+  },
+  /* ------------------------------------------------------------------------------------------------------------ */
+  {
+    path: 'searchbyname',
+    loadChildren: () => import('./pages/searchbyname/searchbyname.module').then( m => m.SearchbynamePageModule)
+  },
+  /* ------------------------------------------------------------------------------------------------------------ */
+  {
+    path: 'searchbytag',
+    loadChildren: () => import('./pages/searchbytag/searchbytag.module').then( m => m.SearchbytagPageModule)
+  },
+  /* ------------------------------------------------------------------------------------------------------------ */
+  {
+    path: 'userposition/:UserID',
+    loadChildren: () => import('./pages/userposition/userposition.module').then( m => m.UserpositionPageModule)
   }
+  /* ------------------------------------------------------------------------------------------------------------ */
 ];
-
+/* ------------------------------------------------------------------------------------------------------------ */
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
 })
+/* ------------------------------------------------------------------------------------------------------------ */
 export class AppRoutingModule {}
+/* ------------------------------------------------------------------------------------------------------------ */
